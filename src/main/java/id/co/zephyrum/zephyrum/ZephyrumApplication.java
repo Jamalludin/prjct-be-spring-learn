c0536d8fe916302f77dda6a352009835f3ecd24e
jamal <jamalludin@bankraya.co.id>
2023-10-11 10:59:23 +0700

    first commit

---
+++ src/main/java/id/co/zephyrum/zephyrum/ZephyrumApplication.java
@@ -0,0 +1,13 @@
+package id.co.zephyrum.zephyrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZephyrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZephyrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/zephyrum/zephyrum/ZephyrumApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.zephyrum.zephyrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZephyrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
